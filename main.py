7f1c6463dee0cbd9c86a18f32c68d4745a615ba1
pedrogomez2019upb <47305995+pedrogomez2019upb@users.noreply.github.com>
2019-02-22 14:06:12 -0500

    Solución
    Se hizo un cambio radical en varios aspectos:
    1. Se le añadio el tipo de decodificación utf-8 para que no haya errores de compilación
    2.Se le añadió el comentario "Solución" para establecer que el programa cambio de propósito.
    3. El programa ahora funciona para resolver una función cuadrática.
    4.Se añadio la liberia math para que pudiera resolver diferentes procedimientos matemáticos.

--- main.py
+++ main.py
@@ -1,10 +1,19 @@
-#Mi primera Modificación
+# -*- coding: utf-8 -*-
+#Solución
 import math
-b=int(input("Bienvenido a este programa , el cual te permitirá saber el cuadrado de un número entero b. También te dirá como calcular la raíz cuadrada d. Por favor ingresa el valor b: "))
-d=int(input("Ahora ingresa el valor D a evaluar: "))
-respuestab= pow(b,2)
-respuestad= math.sqrt(d)
+a= int(input("Hola! Con este programa puedes resolver una ecuación cuadrática. Por favor ingresa el primer valor: "))
+b= int(input("Por favor ingresa el valor b: "))
+c=int(input("Finalmente ingresa el tercer valor (C):"))
 
-print("El cuadrado del número {} es: {} y la raíz de {} es {} . Gracias por utilizar el programa.".format(b,respuestab,d,respuestad))
+d = b**2-4*a*c
 
+if d < 0:
+    print "Lo siento , esta ecuación no tiene alguna solución."
+elif d == 0:
+    x = (-b+math.sqrt(b**2-4*a*c))/2*a
+    print "Este ejercicio tiene dos soluciones. Dame un segundo mientras que lo resuelvo...", x
+else:
+    x1 = (-b+math.sqrt(b**2-4*a*c))/2*a
+    x2 = (-b-math.sqrt(b**2-4*a*c))/2*a
+    print ("Los resultados de esta ecuación cuadrática es {} y {} . Gracias por utilizar este programa.".format(x1,x2))
 #Trabajo creado por Pedro Felipe Gómez ; ID : 000396221
